ae17a2a9bfc174268bb4bca06ff664a43d8ab9cd
Mark Rea <markrea366@gmail.com>
2020-06-29 16:37:37 +0100

    Updates

---
+++ precache-manifest.6fc5ff5ad79afef03328c71f68d49dec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135379691a740ec89dc32dac679e2407",
+    "url": "/https.;//markrea.github.io/tech-test/index.html"
+  },
+  {
+    "revision": "4b146ad8dd0e46b79b17",
+    "url": "/https.;//markrea.github.io/tech-test/static/css/main.12a3b744.chunk.css"
+  },
+  {
+    "revision": "f401b2dbaa72a8b3b29a",
+    "url": "/https.;//markrea.github.io/tech-test/static/js/2.ea3fad5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https.;//markrea.github.io/tech-test/static/js/2.ea3fad5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b146ad8dd0e46b79b17",
+    "url": "/https.;//markrea.github.io/tech-test/static/js/main.183047a1.chunk.js"
+  },
+  {
+    "revision": "0ab1becb3bc073d125e6",
+    "url": "/https.;//markrea.github.io/tech-test/static/js/runtime-main.38f1c252.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77546f2805e590a6a0214b7e19ebc4db.js"
+  "/https.;//markrea.github.io/tech-test/precache-manifest.6fc5ff5ad79afef03328c71f68d49dec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https.;//markrea.github.io/tech-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
